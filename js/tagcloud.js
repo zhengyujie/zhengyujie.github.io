6cfe98d1ad8934d4691f572d2b0f5d3d397e2e0f
zhengyujie <zhengyujie97@126.com>
2019-08-20 21:42:33 +0800

    Site updated: 2019-08-20 21:42:33

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -11,11 +11,11 @@ function addLoadEvent(func) {
 addLoadEvent(function() {
    console.log('tag cloud plugin rock and roll!');
    try {
-       TagCanvas.textFont = 'Trebuchet MS, Helvetica';
+       TagCanvas.textFont = 'Helvetica';
        TagCanvas.textColour = '#333';
-       TagCanvas.textHeight = 20;
-       TagCanvas.outlineColour = '#E2E1D1';
-       TagCanvas.maxSpeed = 0.1;
+       TagCanvas.textHeight = 15;
+       TagCanvas.outlineColour = '#E2E1C1';
+       TagCanvas.maxSpeed = 0.03;
        TagCanvas.outlineMethod = 'block';
        TagCanvas.minBrightness = 0.2;
        TagCanvas.depth = 0.92;
